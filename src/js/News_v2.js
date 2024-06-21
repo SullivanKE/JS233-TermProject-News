61017ce3381d7964343fdaef289264e55264ce64
SullivanKE <Katelyn135@gmail.com>
2024-06-21 13:37:55 -0700

    Replaced init method. Adding in new refactor components to the constructor.

--- src/js/News_v2.js
+++ src/js/News_v2.js
@@ -1,108 +1,47 @@
 import './General.js';
-import { minutesSince } from './DateFunc.js';
-import { ApiController } from './ApiController.js';
+
 import { ArticleModal } from './ArticleModal.js';
 import { SummaryModal } from './SummaryModal.js';
-import Debug from './Debug';
-import LocalStorage from './LocalStorage.js';
 import { DisplayController } from './displayController.js';
-import { StorageList } from './StorageList.js';
-import  Url  from './Url';
-import NewsFeedApi from './NewsFeedApi.js';
+
+import NewsFeedApi from './NewsFeedApi';
+import NewsArticleApi from './NewsArticleApi'
 
 window.NewsFeedApi = NewsFeedApi;
-window.Url = Url;
-console.log("This is the new console.log windows");
+window.NewsArticleApi = NewsArticleApi;
 
 class News {
     constructor() {
-        this.debugging = true;
-        this.prefix = "news.js";
-        this.debug = new Debug(this.prefix, this.debugging);
-
-        let LocalStorageParams = 
-        {
-            defaults: {
-            "top-article": new Array(),
-            "allnews-article-storage": {lastFetch: new Date(), stories: null} // TODO: sets the default storage to when this is called
-            }
-        };
-
-        this.fetchtime = 15; // Time in minutes before a new refresh should be done.
+        this.newsFeedApi = new NewsFeedApi(NEWS_FEED_API_TOKEN);
+        this.newsArticleApi = new NewsArticleApi(NEWS_ARTICLE_API_TOKEN);
     
-
+        // TODO: These will be replaced eventually in the refactor
         this.articleModal = new ArticleModal();
         this.summaryModal = new SummaryModal();
         this.displayController = new DisplayController();
-        this.localStorage = new LocalStorage(LocalStorageParams);
-        this.apiController = new ApiController();
 
-        this.newsFeedApi = new NewsFeedApi(API_TOKEN);
+        // Get the news stories
+        /// Get all news
+        let allNews = this.newsFeedApi.getAllNews();
 
-        this.favoriteStorage = new StorageList({prefix: "Favorites-storage-"});
-        this.articleStorage = new StorageList({prefix: "Article-storage-"});
+        /// Get top stories
+        let topStories = this.newsFeedApi.getTopStories();
 
-        this.debug.debug("Modal Header", document.querySelector("#modalHeader"));
-
-        //this.apiTest();
-        //this.openStory("");
-        //this.openSummary("");
-
-        // Clear all saved information if there is a problem.
-        //this.saveController.clearAll();
-
-        this.init();
-
-    }
-    async init() {
-        // Get what is saved
-        let allNews = this.localStorage.getValue("allnews-article-storage");
-        let topStories = this.localStorage.getValue("top-article");
-        let favorites = this.favoriteStorage.getAllItems();
-
-        this.debug.debug("allNews", allNews);
-        this.debug.debug("top stories", topStories);
-
-        this.debug.debug("Time since test", minutesSince(allNews.lastFetch));
-
-        // If there are no articles in allNews storage, or it is time to fetch based on the value stored in saveController
-        if (allNews.stories == null || 
-            allNews.stories.length == 0 || 
-            minutesSince(allNews.lastFetch) >= this.fetchtime) {
-                
-                allNews = await this.newsFeedApi.getAllNews();
-                //let url = new Url(SERVER_URL + 'all',{locale: 'us', language: 'en', api_token: API_TOKEN });
-                //allNews = await this.apiController.fetchArticle(url.toString());
-                this.debug.debug("just after the fetch", allNews);
-                this.localStorage.setValue("allnews-article-storage", {lastFetch: new Date(), stories: allNews.data});
-                allNews = this.localStorage.getValue("allnews-article-storage");
-        }
-
-        // If there are no articles in topStories storage, or it is time to fetch based on the value stored in saveController
-        if (topStories.stories == null || 
-            topStories.stories.length == 0 || 
-            minutesSince(topStories.lastFetch) >= this.fetchtime) {
-                
-                topStories = await this.newsFeedApi.getTopStories();
-                //let url = new Url(SERVER_URL + 'top',{locale: 'us', language: 'en', api_token: API_TOKEN});
-                //topStories = await this.apiController.fetchArticle(url.toString());
-                this.debug.debug("just after the fetch for top stories", topStories);
-                this.localStorage.setValue("top-article", {lastFetch: new Date(), stories: topStories.data});
-                topStories = this.localStorage.getValue("top-article");
-        }
+        /// Get favorites
+        // TODO: Favorites class to handle favorites
 
 
-        this.debug.debug("allNews", allNews);
-        this.debug.debug("top stories", topStories);
-
-        // Send what we have off to the display controller
-        this.displayController.displayFavorites(favorites);
+        // Display the news stories
+        // TODO: Replace display controller with the new components
+        //this.displayController.displayFavorites(favorites);
         this.displayController.displayTopStories(topStories.stories);
         this.displayController.displayContent(allNews.stories);
 
         // Add event handlers to things
         this.addEventHandlers();
+
     }
+
     async openStory(url, uuid) {
         
         this.debug.debug("openStory call", url);
