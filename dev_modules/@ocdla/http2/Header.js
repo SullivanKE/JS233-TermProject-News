43a6576631d2206e7b538a3669130808a3e7b213
SullivanKE <Katelyn135@gmail.com>
2024-06-27 09:35:28 -0700

    Added a header class to eventually perform some of the work in HttpClient's send() method.

---
+++ dev_modules/@ocdla/http2/Header.js
@@ -0,0 +1,5 @@
+/* TODO
+Add a Header class to to your http2 package with a constructor(name,value) that performs the work currently in send() 
+for retrieving the value of a HTTP header; or in the case of a header like Cache-Controlcan return a multi-value header 
+as an object from which you can retrieve a specific value (i.e., max-age).
+*/
