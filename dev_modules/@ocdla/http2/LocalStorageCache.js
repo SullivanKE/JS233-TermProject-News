c08ae6ef446ef3591c9dbcdbdb699d00ea01b660
SullivanKE <Katelyn135@gmail.com>
2024-06-29 12:44:36 -0700

    Added in the cyrb53 hasher to obfuscate the key used in local storage.

--- dev_modules/@ocdla/http2/LocalStorageCache.js
+++ dev_modules/@ocdla/http2/LocalStorageCache.js
@@ -11,7 +11,7 @@ export default class LocalStorageCache {
 
     static put(req, httpResp) {
         let resp = LocalStorageResponse.fromHttpResponse(httpResp);
-        let key = req.method + req.url;
+        let key = this.cyrb53(req.method + req.url);
         resp.then( resp => {
             resp.addHeader("Date", new Date().toUTCString());
             resp.addHeader("Cache-Control", "public, max-age="+LocalStorageCache.REFRESH_INTERVAL);
@@ -27,7 +27,7 @@ export default class LocalStorageCache {
     static get(req) {
         // Req is the full Request object. We are only interested in the URL at this point as it is the key used in our cache.
         // The method that stores however adds the method to the key. 
-        let key = req.method + req.url;
+        let key = this.cyrb53(req.method + req.url);
 
         const localStorageParams = {
             defaults: {
@@ -47,6 +47,21 @@ export default class LocalStorageCache {
         
         return null;
 
+    }
+
+    static cyrb53(str, seed = 0) {
+        let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+        for(let i = 0, ch; i < str.length; i++) {
+            ch = str.charCodeAt(i);
+            h1 = Math.imul(h1 ^ ch, 2654435761);
+            h2 = Math.imul(h2 ^ ch, 1597334677);
+        }
+        h1  = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+        h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+        h2  = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+        h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+    
+        return 4294967296 * (2097151 & h2) + (h1 >>> 0);
     }
 
     
